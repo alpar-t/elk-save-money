2fb2a025a19541f6bbd4a8b45414ca6707921d5f
Alpar Torok <torokalpar@gmail.com>
2016-11-16 18:36:36 +0200

    Complete sample input script to push into elk

--- sample_prepare_input.sh
+++ sample_prepare_input.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+#!/bin/bash 
+
+set -e
+
 if [ -z "$1" ] ; then 
     echo "Usage $0 <<path to xls>>"
+    exit 1
 fi
 
 tmpfile=$(mktemp  /tmp/$0.XXXXXX.csv)
-trap "rm -v -f $tmpfile >&2" 0 2 3 15
+trap "rm -f $tmpfile >&2" 0 2 3 15
 
 check_tool() {
-    if which ssconvert 2>&1 > /dev/null ; then
+    if which ssconvert > /dev/null 2>&1; then
         return 0
     else
         echo "Can not find '$1'. $2"
@@ -21,7 +25,21 @@ if ! check_tool $SSCONVERT "Please install gnumeric" ; then
    exit 1;
 fi;
 
+# Convert from input XLS
 ssconvert  $1 $tmpfile
 
-grep '^[0-9]\{2\}\/[0-9]\{2\}\/[0-9]\{4\}' $tmpfile > sample.csv 
-cp -v $tmpfile orig.csv 
+# Parse header information
+OWNER=`grep '^"Nume client:' $tmpfile | cut -d, -f2 | sed -e 's/"//g'`
+ACCOUNT_NO=`grep '^"Cod IBAN:' $tmpfile | cut -d, -f2 | sed -e 's/"//g'`
+CURRENCY=`grep '^"Cod IBAN:' $tmpfile | cut -d, -f6 | sed -e 's/"//g'`
+
+
+# 
+# 1. Filter out anything that does not start with a date ( the header )
+# 2. Append the information parsed from the header to each entry
+# 3. Push the result to logstash
+grep '^[0-9]\{2\}\/[0-9]\{2\}\/[0-9]\{4\}' $tmpfile \
+    | sed "s/\$/,$OWNER,$ACCOUNT_NO,$CURRENCY/" \
+    | ncat localhost 6006
+
+echo Successfully processed account owned by  $OWNER
