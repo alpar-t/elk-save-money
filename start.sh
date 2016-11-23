6ccc8bb02c68f0924f4b16d316c27d8d8da6a1a6
Alpar Torok <alpar.torok@devfactory.com>
2016-11-23 04:42:02 +0200

    Ability to start in "develop" mode
    Allows for live editing of the config files on the host and these are
    reflected instantly in the container.

--- start.sh
+++ start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-docker create -p 5601:5601 -p 6006:6006 -v elk-save-money:/var/lib/elasticsearch  --name elk-save-money elk-save-money
+
+set -e
+
+if `echo $@|grep -q '\-\-develop'` ; then 
+    # reflect local conf changes in the container instantly
+    DEVELOP="-v $PWD/logstash.conf.d:/etc/logstash/conf.d"
+    echo "Starting in develop mode"
+fi
+echo "Creating container"
+docker create -p 5601:5601 -p 6006:6006 -v elk-save-money:/var/lib/elasticsearch $DEVELOP  --name elk-save-money elk-save-money
 if [ -f finances.tar.gz ] ; then 
+    echo "Restoring existing data"
     docker run --rm --volumes-from elk-save-money -v $(pwd):/backup elk-save-money tar xzf /backup/finances.tar.gz
 fi
+echo "Starting container"
 docker start elk-save-money
+echo "Everything started successfully"
--- stop.sh
+++ stop.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
+echo "Stopping and extracting data..."
 docker stop elk-save-money
 docker run --rm --volumes-from elk-save-money -v $(pwd):/backup elk-save-money tar cfz /backup/finances.tar.gz /var/lib/elasticsearch
+echo "Cleaning up ..."
 docker rm -f elk-save-money
 docker volume rm elk-save-money
-
+echo "Everything stopped"
